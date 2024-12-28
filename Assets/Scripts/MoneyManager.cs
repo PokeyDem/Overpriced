26412c8683113f94aa103a6ad3a425d89a18e3a4
MTF <116456197+M-T-F@users.noreply.github.com>
2024-12-28 02:41:12 +0100

    Monej v0.1

---
+++ Assets/Scripts/MoneyManager.cs
@@ -0,0 +1,36 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoneyManager : MonoBehaviour {
+
+    public static MoneyManager MoneyManagerInstance;
+    
+    private int _money;
+    
+    public void Awake() {
+        if (MoneyManagerInstance == null) {
+            MoneyManagerInstance = new MoneyManager();
+            DontDestroyOnLoad(MoneyManagerInstance);
+                
+        }
+    }
+
+    public int GetCurrentMoney() {
+        return _money;
+    }
+
+    public void PutMoney(int amount) {
+        _money += amount;
+    }
+
+    public int GetAmountOfMoney(int amount) {
+        if (_money >= amount) {
+            _money -= amount;
+            return amount;
+        }
+
+        throw new Exception("Not Enough Money");
+    }
+}
