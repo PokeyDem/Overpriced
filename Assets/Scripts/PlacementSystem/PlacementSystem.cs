d35960968349e1e6cc8355ffce031a3367591b72
Mikołaj Falkowski <116456197+M-T-F@users.noreply.github.com>
2025-01-21 15:20:39 +0100

    build set UP UP AND AWAY

--- Assets/Scripts/PlacementSystem/PlacementSystem.cs
+++ Assets/Scripts/PlacementSystem/PlacementSystem.cs
@@ -2,7 +2,7 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using Cinemachine;
-using UnityEditor.ShaderGraph.Internal;
+//using UnityEditor.ShaderGraph.Internal;
 using UnityEngine;
 using UnityEngine.PlayerLoop;
 
