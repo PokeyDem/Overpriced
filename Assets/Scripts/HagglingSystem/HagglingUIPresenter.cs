36653c7835259fcd2a9555f6521049afbe649e8c
s21147 <s21147@pjwstk.edu.pl>
2025-04-11 20:04:05 +0200

    added HagglingManager to HagglingUIPresenter as var

---
+++ Assets/Scripts/HagglingSystem/HagglingUIPresenter.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HagglingUIPresenter : MonoBehaviour
+{
+    [SerializeField] private HagglingManager _model;
+    [SerializeField] private HagglingUIView _view;
+    private void Awake()
+    {
+        
+    }
+}
