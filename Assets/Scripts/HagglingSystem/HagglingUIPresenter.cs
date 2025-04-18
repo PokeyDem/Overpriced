1cfa7f12f7bc145aa42b87b672802fd6b84e032d
Pietruszkak <s21147@pjwstk.edu.pl>
2025-04-18 20:11:16 +0200

    added hints and npc offer to haggling

--- Assets/Scripts/HagglingSystem/HagglingUIPresenter.cs
+++ Assets/Scripts/HagglingSystem/HagglingUIPresenter.cs
@@ -1,11 +1,17 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class HagglingUIPresenter : MonoBehaviour
 {
     [SerializeField] private HagglingManager _model;
     [SerializeField] private HagglingUIView _view;
+
+    [SerializeField] private Button _sellButton;
+    [SerializeField] private Button _acceptOfferButton;
+    [SerializeField] private Button _denyOfferButton;
+
     private void Awake()
     {
         _model.HagglingInitiated += OnHagglingInitiated;
@@ -13,8 +19,12 @@ public class HagglingUIPresenter : MonoBehaviour
         _model.ItemSold += OnItemSold;
         _model.ItemDenied += OnItemDenied;
         _model.HagglingEnded += OnHagglingEnded;
-        _model.NrOfChancesChanged += OnNrOfChancesChanged;
+        _model.NrOfAttemptsChanged += OnNrOfAttemptsChanged;
         _model.UIDisabled += OnUIDisabled;
+        _model.AttemptsDepleted += OnAttemptsDepleted;
+
+        _acceptOfferButton.onClick.AddListener(_model.SellItem);
+        _denyOfferButton.onClick.AddListener(_model.DenySell);
     }
 
     private void OnHagglingInitiated()
@@ -24,8 +34,10 @@ public class HagglingUIPresenter : MonoBehaviour
         _view.UpdateSliderValue(_model.GetCurrentPrice());
         _view.UpdatePriceField(_model.GetCurrentPrice());
         _view.UpdateProcentField(_model.GetBasePrice(), _model.GetCurrentPrice());
-        _view.UpdateChancesLeftField(_model.GetNrOfChancesLeft());
+        _view.UpdateAttemptsLeftField(_model.GetNrOfAttemptsLeft());
         _view.UpdateNpcOffer(_model.GetNpcOffer());
+        _view.ToggleHaggleWindow(true);
+        _view.ToggleAcceptDenyOfferWindow(false);
         _view.UpdateUIInteraction(true);
         _view.SetActiveHagglingUI(true);
     }
@@ -37,9 +49,12 @@ public class HagglingUIPresenter : MonoBehaviour
         _view.UpdateProcentField(_model.GetBasePrice(), _model.GetCurrentPrice());
     }
     private void OnItemSold() =>
-        _view.UpdateResultField("Sold",Color.green);
-    private void OnItemDenied() =>
-        _view.UpdateResultField("Failed", Color.red);
+        _view.UpdateResultField("A fair exchange, I’d say.", Color.green);
+    private void OnItemDenied() 
+    {
+        _view.UpdateResultField("This is a waste of time. We’re done here.", Color.red);
+        _view.UpdateAttemptsLeftField(_model.GetNrOfAttemptsLeft());
+    }
 
     private void OnHagglingEnded() =>
         _view.SetActiveHagglingUI(false);
@@ -58,13 +73,44 @@ public class HagglingUIPresenter : MonoBehaviour
         _view.UpdateSliderValue(_model.GetCurrentPrice());
         _view.UpdateProcentField(_model.GetBasePrice(), _model.GetCurrentPrice());
     }
-    private void OnNrOfChancesChanged()
+    private void OnNrOfAttemptsChanged()
     {
-        _view.UpdateChancesLeftField(_model.GetNrOfChancesLeft());
+        _view.UpdateAttemptsLeftField(_model.GetNrOfAttemptsLeft());
         _view.UpdateNpcOffer(_model.GetNpcOffer());
+        UpdateHint();
+        _view.UpdateSlider(_model.GetNpcOffer(), _model.GetCurrentPrice());
     }
     private void OnUIDisabled()
     {
         _view.UpdateUIInteraction(false);
+    }
+
+    private void UpdateHint()
+    {
+        int basePrice=_model.GetBasePrice();
+        int currentPrice=_model.GetCurrentPrice();
+        float maxAcceptableMarkup=_model.GetMaxAcceptableMarkup();
+        float percentageIncreaseDecimal = (float)currentPrice / basePrice - 1f;
+        float difference = percentageIncreaseDecimal - maxAcceptableMarkup;
+        if (difference > maxAcceptableMarkup/2)
+        {
+            _view.UpdateResultField("That price is outrageous!", Color.yellow);
+        }
+        else if (difference > maxAcceptableMarkup / 3)
+        {
+            _view.UpdateResultField("I won’t pay that much.", Color.yellow);
+        }
+        else if (difference > 0)
+        {
+            _view.UpdateResultField("It’s slightly over my limit.", Color.yellow);
+        }
+
+    }
+    private void OnAttemptsDepleted()
+    {
+        _view.ToggleHaggleWindow(false);
+        _view.ToggleAcceptDenyOfferWindow(true);
+        _view.TogglePriceInteraction(false);
+        _view.UpdateResultField("I've lost my patience. This is my last offer. Take it or stop wasting my time.", Color.yellow);
     }
 }
