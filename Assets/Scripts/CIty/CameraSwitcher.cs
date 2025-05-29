7b9944988e09840239b659faec0c6ae2e37a7628
PokeyDem-study <s24439@pjwstk.edu.pl>
2025-05-29 16:15:38 +0200

    Fixed scene loader

--- Assets/Scripts/CIty/CameraSwitcher.cs
+++ Assets/Scripts/CIty/CameraSwitcher.cs
@@ -14,7 +14,7 @@ public class CameraSwitcher : SingletonDontDestroyOnLoad<CameraSwitcher>{
 
     private new void Awake(){
         base.Awake();
-        if (SceneManager.loadedSceneCount == 1) 
+        if (SceneManager.loadedSceneCount  < 2) 
             SceneManager.LoadScene("City_scene", LoadSceneMode.Additive);
         _shopCam.enabled = true;
         _cityCam.enabled = false;
