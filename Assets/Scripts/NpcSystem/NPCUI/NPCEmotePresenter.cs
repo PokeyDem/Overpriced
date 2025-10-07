d630e01fc60347d597b5a6a416faf92672e83fb9
Pietruszkak <s21147@pjwstk.edu.pl>
2025-10-07 17:47:22 +0200

    implemented emotes into npc behaviour

--- Assets/Scripts/NpcSystem/NPCUI/NPCEmotePresenter.cs
+++ Assets/Scripts/NpcSystem/NPCUI/NPCEmotePresenter.cs
@@ -6,6 +6,10 @@ public class NPCEmotePresenter
 {
     private NpcBehaviour _npcBehaviour;
     private UpdateText _updateText;
+    private Sprite _emoteHappy;
+    private Sprite _emoteSad;
+    private Sprite _emoteThinking;
+
     public NPCEmotePresenter(NpcBehaviour npcBehaviour, UpdateText updateText)
     {
         _npcBehaviour = npcBehaviour;
@@ -14,23 +18,33 @@ public class NPCEmotePresenter
         _npcBehaviour.DecidingStarted += OnDecidingStarted;
         _npcBehaviour.ItemSelected += OnItemSelected;
         _npcBehaviour.ItemRejected += OnItemRejected;
+        _emoteHappy = Resources.Load<Sprite>("UI/Emotes/Emote_Happy");
+        _emoteSad = Resources.Load<Sprite>("UI/Emotes/Emote_Sad");
+        _emoteThinking = Resources.Load<Sprite>("UI/Emotes/Emote_Thinking");
     }
 
-    private void OnInitialized() 
+    private void OnInitialized()
     {
-        _updateText.UpdateTextMesh("",Color.white);
-        _updateText.SetActive(true);
+        _updateText.UpdateImageAlpha(0);
     }
 
-    private void OnDecidingStarted() => 
-        _updateText.UpdateTextMesh("?",Color.yellow);
+    private void OnDecidingStarted()
+        {
+            _updateText.UpdateImage(_emoteThinking);
+            _updateText.UpdateImageAlpha(1);
+        }
+        
 
 
-    private void OnItemSelected() =>
-        _updateText.UpdateTextMesh(":)", Color.green);
+    private void OnItemSelected()
+    {
+        _updateText.UpdateImage(_emoteHappy);
+        _updateText.UpdateImageAlpha(1);
+    }
+     
 
     private void OnItemRejected() =>
-        _updateText.UpdateTextMesh(":(", Color.red);
+        _updateText.UpdateImage(_emoteSad);
 
 
 
