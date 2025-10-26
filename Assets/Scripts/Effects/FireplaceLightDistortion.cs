df08c3da0a83efc1f946b8402357fbe2823ff0a8
Aleksandr Maslov <163117728+PokeyDem-study@users.noreply.github.com>
2025-10-26 17:21:21 +0100

    Added fireplace particle system and fixed potion glass material

---
+++ Assets/Scripts/Effects/FireplaceLightDistortion.cs
@@ -0,0 +1,31 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Random = UnityEngine.Random;
+
+public class FireplaceLightDistortion : MonoBehaviour
+{
+   public float intensityBase = 2.0f;
+   public float intensityAmplitude = 1.0f;
+   public float flickerSpeed = 2.0f;
+   public float colorShiftStrength = 0.1f;
+
+   private Light _fireLight;
+   private float _timeOffset;
+
+   void Start()
+   {
+      if (_fireLight == null)
+         _fireLight = GetComponent<Light>();
+      _timeOffset = Random.Range(0f, 100f);
+   }
+
+   void Update()
+   {
+      float time = Time.time * flickerSpeed + _timeOffset;
+      
+      float noise = Mathf.PerlinNoise(time, 0.0f);
+      _fireLight.intensity = intensityBase + noise * intensityAmplitude;
+   }
+}
