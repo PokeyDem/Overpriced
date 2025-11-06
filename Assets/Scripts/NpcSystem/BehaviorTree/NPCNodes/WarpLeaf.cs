8a20d73cec1ee35bfca2a9c157e9f1dc4a029e10
Pietruszkak <s21147@pjwstk.edu.pl>
2025-11-06 03:38:32 +0100

    changed rotation of warping to the shop

--- Assets/Scripts/NpcSystem/BehaviorTree/NPCNodes/WarpLeaf.cs
+++ Assets/Scripts/NpcSystem/BehaviorTree/NPCNodes/WarpLeaf.cs
@@ -17,6 +17,7 @@ public class WarpNode : Node
     public override NodeState Evaluate()
     {
         _agent.Warp(_context.Target);
+        _agent.transform.LookAt(_context.Target + new Vector3(0, 0, -1));
         return NodeState.SUCCESS;
     }
 }
