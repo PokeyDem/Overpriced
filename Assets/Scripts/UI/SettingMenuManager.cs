21b22be74d726f27f3051f508557b568f641a814
Aleksandr Maslov <163117728+PokeyDem-study@users.noreply.github.com>
2025-12-08 14:41:34 +0100

    Added graphic change to the setting

--- Assets/Scripts/UI/SettingMenuManager.cs
+++ Assets/Scripts/UI/SettingMenuManager.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 using UnityEngine.Audio;
 using UnityEngine.UI;
@@ -9,6 +10,7 @@ public class SettingMenuManager : MonoBehaviour
 {
     [SerializeField] private AudioMixer audioMixer;
     [SerializeField] private Slider volumeSlider;
+    [SerializeField] private TMP_Dropdown qualityDropdown;
 
     private void Start()
     {
@@ -18,11 +20,30 @@ public class SettingMenuManager : MonoBehaviour
             volumeSlider.SetValueWithoutNotify(0.5f);
             SetMusicVolume(0.5f);
         }
+        
+        qualityDropdown.ClearOptions();
+      
+        string[] qualityNames = QualitySettings.names;
+     
+        List<string> options = new List<string>(qualityNames);
+        qualityDropdown.AddOptions(options);
+     
+        qualityDropdown.value = QualitySettings.GetQualityLevel();
+        qualityDropdown.RefreshShownValue();
+        
+        qualityDropdown.onValueChanged.AddListener(SetQuality);
     }
 
     public void SetMusicVolume(float sliderValue)
     {
         float dB = (sliderValue > 0.0001f) ? Mathf.Log10(sliderValue) * 20 : -80f;
         audioMixer.SetFloat("MasterVolume", dB);
+    }
+    
+    public void SetQuality(int index)
+    {
+        QualitySettings.SetQualityLevel(index);
+     
+        UnityEngine.Rendering.OnDemandRendering.renderFrameInterval = 1;
     }
 }
